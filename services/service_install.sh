c7a7793b263c15bddd4793d671fe875e43a1e2bb
albertoccelli <albertoccelli@gmail.com>
2023-09-19 14:36:50 +0200

    Install script edited

--- services/service_install.sh
+++ services/service_install.sh
@@ -1,6 +1,22 @@
+
 #!/bin/bash
 
 TO_INSTALL=$1
 INSTALL_LOCATION=~/.config/systemd/user/
 
 echo "Installing service $TO_INSTALL into location $INSTALL_LOCATION"
+# copy service into user install location
+echo "Copying files into install location"
+cp $TO_INSTALL $INSTALL_LOCATION
+echo "Enabling service"
+systemctl --user enable $TO_INSTALL
+
+echo "Install complete!"
+read -p "Do you want to reboot the system now? (y/n)" answer
+if [ $answer == "y" ] ; then
+	echo "Rebooting..."
+	sudo reboot now
+elif [ $answer == "n" ] ; then
+	echo "Starting service $TO_INSTALL"
+	systemctl --user start $TO_INSTALL
+fi
