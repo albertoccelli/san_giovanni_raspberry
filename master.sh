c62aa73047034b074aa97a4522ef382683793489
albertoccelli <albertoccelli@gmail.com>
2023-09-21 16:25:18 +0200

    Updated utils.py

--- master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# start audio player
-python /home/a.occelli/sm_demo/bt_device.py
-sleep 2
-python /home/a.occelli/sm_demo/master.py
-
--- utils.py
+++ utils.py
@@ -1,9 +1,6 @@
 import subprocess
-from get_sinks import getSinks, getPaths
 import yaml
 import time
-
-jack_sink = getSinks()[0]
 
 
 def set_spkr_volume_max():
@@ -64,3 +61,40 @@ def check_player():
             return True
     else:
         print("Error")
+
+
+def getSinks():
+    command = "pactl list sinks"
+
+    output = subprocess.check_output(command, shell=True, text = True)
+    output_lines = output.splitlines()
+
+    sink = 0
+    names = []
+    paths = []
+    for l in output_lines:
+        if "Name" in l:
+            names.append(l.split(":")[-1].replace(" ",""))
+        if "bluez.path" in l:
+            paths.append(l.split(":")[-1].replace('"', '').replace(' ', ''))
+    return(names)
+
+
+def getPaths():
+        command = "pactl list sinks"
+
+        output = subprocess.check_output(command, shell=True, text = True)
+        output_lines = output.splitlines()
+
+        sink = 0
+        names = []
+        paths = []
+        for l in output_lines:
+                if "Name" in l:
+                        names.append(l.split(":")[-1].replace(" ",""))
+                if ".path" in l:
+                        paths.append(l.split("=")[-1].replace('"', '').replace(' ', ''))
+        return(paths)
+
+
+jack_sink = getSinks()[0]
