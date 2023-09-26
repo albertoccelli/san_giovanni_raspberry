d5f02ac8f089543b19763e7449de504dba7c9c5f
albertoccelli <albertoccelli@gmail.com>
2023-09-26 18:04:19 +0200

    Update main bash script

--- main.sh
+++ main.sh
@@ -10,12 +10,11 @@ echo $num_files
 num_to_remove=$((num_files - num_to_keep))
 
 if [ $num_to_remove -gt 0 ]; then
-  # Rimuovi i file in eccesso
   files_to_remove=$(echo "$files" | tail -n $num_to_remove)
   rm $files_to_remove
-  echo "$num_to_remove file rimossi."
+  echo "$num_to_remove files removed."
 else
-  echo "Nessun file da rimuovere."
+  echo "No log file to remove."
 fi
 
 echo Logging into logs/log_$TIMESTAMP.txt
