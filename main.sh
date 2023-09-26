c4f4846997edf491266048fc49f58c6dda4b54f7
albertoccelli <albertoccelli@gmail.com>
2023-09-26 15:12:16 +0200

    Services adjusted

---
+++ main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
+# get into logs directory
+cd /home/a.occelli/sm_demo/logs
+
+files=$(ls -t)
+echo $files
+num_to_keep=10
+num_files=$(ls | wc -l)
+echo $num_files
+num_to_remove=$((num_files - num_to_keep))
+
+if [ $num_to_remove -gt 0 ]; then
+  # Rimuovi i file in eccesso
+  files_to_remove=$(echo "$files" | tail -n $num_to_remove)
+  rm $files_to_remove
+  echo "$num_to_remove file rimossi."
+else
+  echo "Nessun file da rimuovere."
+fi
+
+echo Logging into logs/log_$TIMESTAMP.txt
+python /home/a.occelli/sm_demo/main.py >> log_$TIMESTAMP.txt 2>&1
