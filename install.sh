af22aef13b2e9b103916c138ae480803ec85fc92
albertoccelli <albertoccelli@gmail.com>
2023-09-27 12:08:25 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -5,13 +5,20 @@ curwd=$PWD
 
 # Install sm_demo script package
 # Update package list
+echo "Upgrading the system..."
 sudo apt-get update
 
 # Upgrade the system
 sudo apt-get upgrade
+echo "Done"
 
 # Add user
 sudo adduser $USER bluetooth
+
+# Copy modified bluetooth service into systemd
+echo "Copying modified bluetooth service into system..."
+sudo cp $PWD/services/bluetooth.service /etc/systemd/system/
+echo "Done!"
 
 # Reload systemd daemon
 sudo systemctl daemon-reload
@@ -27,4 +34,6 @@ systemctl --user enable pulseaudio
 systemctl --user start pulseaudio
 
 # Install dependencies for Python
+echo "Installing Python dependencies..."
 pip install -r $PWD/requirements.txt
+echo "Done!"
