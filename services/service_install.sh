04f9825c96f4954cfa8a083e48cce7005f80a70b
albertoccelli <albertoccelli@gmail.com>
2023-10-16 15:23:35 +0200

    Update services to be easily installed

--- services/service_install.sh
+++ services/service_install.sh
@@ -5,8 +5,8 @@ INSTALL_LOCATION=~/.config/systemd/user/
 
 echo "Installing service $TO_INSTALL into location $INSTALL_LOCATION"
 # copy service into user install location
-echo "Copying files into install location"
-cp $TO_INSTALL $INSTALL_LOCATION
+echo "Copying files into install location. Env variable: $SM_DIR"
+sed 's#/path/to/#'$SM_DIR'/#g' $TO_INSTALL > $INSTALL_LOCATION/$TO_INSTALL
 echo "Enabling service"
 systemctl --user enable $TO_INSTALL
 
