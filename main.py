d68493a1f59cc2946d44ec1db7e9588d6f0da147
albertoccelli <albertoccelli@gmail.com>
2023-11-23 08:58:58 +0100

    Bugfix

--- main.py
+++ main.py
@@ -22,17 +22,30 @@ __status__ = "Dev"
 import RPi.GPIO as GPIO
 import time
 import os
-
+import subprocess
 
 from config import *
-from utils import curwd, audio_prompt, print_datetime
+from utils import curwd, audio_prompt, print_datetime, get_sinks
 
 GPIO.setup(button_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button_5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 running = False
 
+def get_running():
+    run = subprocess.Popen(["systemctl", "--user", "status", "player.service"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    run.wait()
+    stdout, stderr = run.communicate()
+    status = str(stdout).split("Active: ")[-1].split("\\n")[0]
+    if "inactive" in status.lower():
+        return False
+    else:
+        if "active" in status.lower():
+            return True
+        else:
+            return None
 
 def toggle_standby(channel):
+    global running
     pressed_time = time.time()
     while GPIO.input(button_1) == GPIO.HIGH:
         if GPIO.input(button_5) == GPIO.HIGH:
@@ -40,6 +53,7 @@ def toggle_standby(channel):
             return
         elapsed = time.time()-pressed_time
         if elapsed >= 4:
+            print(running)
             standby()
             return
 
@@ -66,11 +80,13 @@ def reboot():
 
 def standby():
     global running
+    running = get_running()
     if running:
         print_datetime("Stopping demo")
         os.system("systemctl --user stop player")
-        os.system("sudo systemctl stop bluetooth")
         audio_prompt(f"{curwd}/prompts/eng/standby.wav")
+        time.sleep(1)
+        os.system("sudo systemctl stop bluetooth")
     else:
         print_datetime("Starting demo")
         os.system("systemctl --user start player")
@@ -83,14 +99,20 @@ GPIO.add_event_detect(button_5, GPIO.RISING, callback=reboot_button, bouncetime=
 
 
 def main():
+    global running
     print_datetime("+++ WELCOME TO THE SANMARCO INSTORE DEMO +++")
     print_datetime(f"Version: {__version__}")
     os.system(f"pactl set-sink-volume 0 {fr_volume}%")
     os.system("systemctl --user stop player")
     audio_prompt(f"{curwd}/prompts/startup.wav")
+    p_sinks = 1
     try:
         while True:
-            time.sleep(10)
+            n_sinks = len(get_sinks())
+            if running and n_sinks == 1 and p_sinks != 1:
+                standby()
+            time.sleep(1)
+            p_sinks = n_sinks
             pass
     except KeyboardInterrupt:
         GPIO.cleanup()
